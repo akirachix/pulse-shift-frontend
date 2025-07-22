ca65957f730e7bbb9c7c0ca072f698783fe24dbf
lwam <lwambsrat@gmail.com>
2025-07-22 14:28:40 +0300

    update some changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DashboardLayout from './sharedcomponent/Layout';
 import Sales from './Sales/';
+import ProductPage from './Products';
 import ComingSoon from './404';
 
 
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter future={{ v7_relativeSplatPath: true }}>
       <Routes>
           <Route element={<DashboardLayout />}>
+            <Route path="products" element={<ProductPage/>}/>
             <Route path="/sales" element={<Sales/>} />
             <Route path="*" element={<ComingSoon />} />
            
